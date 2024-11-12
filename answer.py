6f9efc372fc752b897400ae399c5f0ef491f8344
Malcolm Turay <98483173+turaymalcolm@users.noreply.github.com>
2024-11-12 18:44:19 +0000

    Create answer.py

---
+++ answer.py
@@ -0,0 +1,30 @@
+# Step 1
+my_list = []
+print(my_list)
+
+# Step 2
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+# Step 3
+my_list.insert(1, 15)
+print(my_list)
+
+# Step 4
+my_list.extend([50, 60, 70])
+print(my_list)
+
+# Step 5
+my_list.pop()
+print(my_list)
+
+# Step 6
+my_list.sort()
+print(my_list)
+
+# Step 7
+index_of_30 = my_list.index(30)
+print(index_of_30)
